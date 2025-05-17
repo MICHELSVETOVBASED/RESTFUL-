e205fc2c155ba32fda9f3a0b43257418ff3b4c67
maksk <wgagaqwt1@gmail.com>
2025-05-17 22:41:06 +0200

    Woo

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,10 @@
+namespace RESTREST_2.Controllers;
+
+public class ProfileController{
+    public string[] Get(){
+        return new string[]{
+            "Graycen",
+            "Cock"
+        };
+    }
+}
---
+++ Models/Profile.cs
@@ -0,0 +1,6 @@
+namespace RESTREST_2.Models;
+
+public class Profile{
+    public int Id{ get; set; }
+    public int Name{ get; set; }
+}
